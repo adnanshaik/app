fa8b1b317fc0579ba2b8e3102af55df2636bc8f8
adnanshaik <68059262+adnanshaik@users.noreply.github.com>
2021-01-27 19:06:16 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,3 @@
 pip3 install -r requirements.txt
 supervisorctl start all
-touch /etc/nginx/sites-available/nginxconf
-cp /chatapp/scripts/nginxconf /etc/nginx/sites-available/nginxconf
-sudo ln -s /etc/nginx/sites-available/nginxconf /etc/nginx/sites-enabled
 sudo systemctl restart nginx
