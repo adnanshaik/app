720244f21286c6c7527d0e4669f24314adbbaa97
adnanshaik <68059262+adnanshaik@users.noreply.github.com>
2021-01-27 18:40:51 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,3 +2,4 @@ sudo apt-get update
 sudo apt-get install -y python3-pip
 sudo apt-get install -y nginx
 nano /etc/nginx/sites-available/nginxconf
+sudo ln -s /etc/nginx/sites-available/nginxconf /etc/nginx/sites-enabled
