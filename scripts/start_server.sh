2490ec19bbaab00db7f33eb67a5a7225eb854def
adnanshaik <68059262+adnanshaik@users.noreply.github.com>
2021-01-27 19:01:01 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,6 @@
 pip3 install -r requirements.txt
 supervisorctl start all
-systemctl start nginx
+touch /etc/nginx/sites-available/nginxconf
+cp /chatapp/scripts/nginxconf /etc/nginx/sites-available/nginxconf
+sudo ln -s /etc/nginx/sites-available/nginxconf /etc/nginx/sites-enabled
+sudo systemctl restart nginx
